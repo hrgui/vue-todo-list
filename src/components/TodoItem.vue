7409baedc21f72bc44763f84389335b6d469bed6
Harman <5001432+hrgui@users.noreply.github.com>
2016-10-23 21:48:59 -0700

    First commit

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input type="checkbox" v-model="todo.completed">
+    <s v-if="todo.completed">
+      {{todo.text}}
+    </s>
+    <span v-if="!todo.completed">
+          {{todo.text}}
+        </span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'todo-item',
+    props: ['todo'],
+    data() {
+      return {};
+    },
+  };
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>Todo List</h1>
+    <ul>
+      <li v-for="(todo, i) in todos">
+        <todo-item :todo="todo"></todo-item>
+        <button @click="removeTodo(todo, i)">Del</button>
+      </li>
+    </ul>
+
+
+    <input type="text" v-model="newTodo">
+    <button @click="addTodo(newTodo)">Add Todo</button>
+
+  </div>
+</template>
+
+<script>
+  import TodoItem from './TodoItem';
+
+  export default {
+    name: 'todo-list',
+    components: { TodoItem },
+    data() {
+      return {
+        newTodo: null,
+        todos: [],
+      };
+    },
+    methods: {
+      addTodo(v) {
+        this.todos.push({ text: v, completed: false });
+        this.newTodo = null;
+      },
+      removeTodo(todo, i) {
+        this.todos.splice(i, 1);
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  li {
+    list-style: none;
+  }
+</style>
